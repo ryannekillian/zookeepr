0021e4a6e53241dea7e515887ecedd730bde1d67
ryannekillian <ryanne.killian@gmail.com>
2020-08-02 12:11:19 -0500

    changed route to app for port

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-
-const { animals } = require('./data/animals');
-
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
+const fs = require("fs");
+const path = require("path");
+const { animals } = require("./data/animals");
+const express = require("express");
+const { ESPIPE } = require("constants");
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-
-
+app.use(express.static("public"));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 app.use('/api', apiRoutes);
-app.use('/api', htmlRoutes);
-app.use(express.static('public'));
-
-router.listen(PORT, () => {
+app.use('/', htmlRoutes);
+app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
